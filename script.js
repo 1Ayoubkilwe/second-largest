b03cf5bf172c88eb4a519928d2a167d6229d7fc1
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-08 11:23:50 +0300

    jawaabo

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+function secondLargest(arr) {
+    
+    let sortedArr = [...new Set(arr)].sort((a, b) => b - a);
+    
+    return sortedArr[1];
+  }
+  
+  
+  console.log(secondLargest([23, 66, 50, 12, 6])); // 50
+  console.log(secondLargest([65, 34, 100, 22, 85])); // 85
+  console.log(secondLargest([67, 22, 19, 12, 80])); // 67
+  
